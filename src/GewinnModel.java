bbde1eacf84b1ffb4f0269942d3dd550f9bf5edd
Nora Morawek <nora.net@morawek.at>
2026-09-23 09:01:13 +0200

    Weitere Methoden hinzugefügt - Klasse GewinnModel fertiggestellt

--- src/GewinnModel.java
+++ src/GewinnModel.java
@@ -22,5 +22,37 @@ public class GewinnModel {
         return rundenErgebnis;
     }
 
+    public void berechneComputerZahl(){
+        this.computerZahl= (int) (Math.random()*9)+1;
+    }
 
+    public void berechneRunde(int spielerZahl){
+        this.spielerZahl = spielerZahl;
+        if(spielerZahl == this.computerZahl){
+            this.rundenErgebnis = 20;
+            this.gesamtPunkte += 20;
+        } else if(spielerZahl -1 == this.computerZahl || spielerZahl<this.computerZahl){
+            this.rundenErgebnis = 5;
+            this.gesamtPunkte += 5;
+        }else {
+            this.rundenErgebnis = -10;
+            this.gesamtPunkte -= 20;
+        }
+    }
+
+    public boolean hatGewonnen(){
+        if(this.gesamtPunkte>= 100){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public boolean hatVerloren(){
+        if(this.gesamtPunkte <= 0){
+            return true;
+        } else{
+            return false;
+        }
+    }
 }
