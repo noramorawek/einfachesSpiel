81c99ad88121e80b6fac55d1805b5e959c160553
Nora Morawek <nora.net@morawek.at>
2026-09-24 13:50:30 +0200

    Kommentar geändert

--- src/GewinnController.java
+++ src/GewinnController.java
@@ -14,6 +14,7 @@ public class GewinnController {
     private class SpielerZahlListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
+            // grewaehlte Zahl
             int spielerZahl;
             try {
                 spielerZahl = Integer.parseInt(view.getSpielerZahl());
