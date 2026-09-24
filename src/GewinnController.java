0f0976395ec548527ec2e7f32c4a5b05b19f5863
Nora Morawek <nora.net@morawek.at>
2026-09-24 10:41:40 +0200

    Controller fertiggestellt

--- src/GewinnController.java
+++ src/GewinnController.java
@@ -10,4 +10,40 @@ public class GewinnController {
         this.model = model;
         this.view = view;
     }
+    private class SpielerZahlListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int spielerZahl;
+            try {
+                spielerZahl = Integer.parseInt(view.getSpielerZahl());
+            } catch (NumberFormatException ex) {
+                return;
+            }
+
+            if (spielerZahl < 1 || spielerZahl > 9) {
+                return;
+            }
+
+            model.berechneComputerZahl();
+            model.berechneRunde(spielerZahl);
+
+            view.zeigeComputerZahl(model.getComputerZahl());
+            view.zeigeRundenErgebnis(model.getRundenErgebnis());
+            view.zeigeGesamtPunkte(model.getGesamtPunkte());
+
+            if (model.hatGewonnen()) {
+                view.zeigeGewonnen();
+            } else if (model.hatVerloren()) {
+                view.zeigeVerloren();
+            }
+        }
+    }
+
+    private class NochEinmalListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            view.zurueckSetzen();
+        }
+    }
+
 }
